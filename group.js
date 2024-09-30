17c80ceb0650d05dd34c84fa37dbd6b6fb81b563
DEMONST3R-CODER <wisdomoseghale7@gmail.com>
2024-09-30 09:22:51 +0100

    Update group.js

--- group.js
+++ group.js
@@ -741,7 +741,7 @@ const {
        return await _0x5e533c.reply("*Huh, I can't kick my Creator!!*");
      }
      await _0x5e533c.bot.groupParticipantsUpdate(_0x5e533c.chat, [_0x4e844a], "remove");
-     await _0x5e533c.send("*Hurray, @" + _0x4e844a.split("@")[0] + " Kicked Succesfully!*", {
+     await _0x5e533c.send("*Hurray,one idiot kicked out successfully 🥰 @" + _0x4e844a.split("@")[0] + " Kicked Succesfully! 😊*", {
        mentions: [_0x4e844a]
      });
    } catch (_0x14d7b9) {
